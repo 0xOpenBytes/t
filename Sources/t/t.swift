12257de711587cfe9540a658f25f80540f4d3508
Leif <zmeriksen@icloud.com>
2023-02-16 18:17:59 -0700

    Promote Error to LocalizedError

--- Sources/t/t.swift
+++ Sources/t/t.swift
@@ -1,10 +1,16 @@
+import Foundation
+
 /// A simple way to test expectations using functions and errors
 public enum t {
     /// Simple Error with a description stating the failure
-    public struct TestError: Error {
+    public struct TestError: LocalizedError {
         public let description: String
         public let lineNumber: Int
         public let functionName: String
         public let fileName: String
+
+        public var errorDescription: String? {
+            "\(description) (file: \(fileName) func: \(functionName)@\(lineNumber))"
+        }
     }
 }
